9edde6678c1b891932baea54d57417421461de6b
Karasaev Magomed <rostovmen95@gmail.com>
2025-07-17 16:32:42 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {ButtonAntd} from "./components/button/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonAntd/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/button/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'antd';
+
+
+
+export const ButtonAntd: React.FC = () => {
+
+    return <Button>
+        click
+    </Button>
+}
+
+
